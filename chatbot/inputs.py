b90e33d2d23a6f7f18fce27853c1ebfd151e7929
David ONeill <mrdavidoneillll@gmail.com>
2023-03-01 21:32:19 +0100

    Add inputs module
    Defines lists of affirmative and negative responses
    
    - TRUE: list of strings representing affirmative responses
    - FALSE: list of strings representing negative responses

---
+++ chatbot/inputs.py
@@ -0,0 +1,12 @@
+"""
+This module defines two lists of strings representing affirmative and 
+negative responses.
+    
+TRUE : list
+    A list of strings representing affirmative responses.
+
+FALSE : list
+    A list of strings representing negative responses.
+"""
+TRUE = ["yes", "y", "si", "yeah", "yep"]
+FALSE = ["no", "n", "nope", "nah"]
